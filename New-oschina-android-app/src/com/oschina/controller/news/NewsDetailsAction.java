a8fa3d4b7fabe1587887fc457a8e8b63bea75cd9
renyuan <renyuan@minephone.com>
2012-12-03 02:01:24 +0800

    upateliab

---
+++ New-oschina-android-app/src/com/oschina/controller/news/NewsDetailsAction.java
@@ -0,0 +1,15 @@
+package com.oschina.controller.news;
+
+import android.content.Context;
+
+import com.androidquery.AQuery;
+import com.youxilua.framework.action.ApiBaseAction;
+
+public class NewsDetailsAction extends ApiBaseAction{
+
+	public NewsDetailsAction(Context ctx, AQuery aq) {
+		super(ctx, aq);
+		// TODO Auto-generated constructor stub
+	}
+
+}
