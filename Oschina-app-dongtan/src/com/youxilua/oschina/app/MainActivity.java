3599cce1dd7b3f03d11027d5ccf16faba9211dbf
youxiachai <youxiachai@gmail.com>
2013-02-04 15:40:30 +0800

    完成对api 的调试

---
+++ Oschina-app-dongtan/src/com/youxilua/oschina/app/MainActivity.java
@@ -0,0 +1,34 @@
+package com.youxilua.oschina.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.FrameLayout;
+
+import com.youxilua.dongtan.R;
+import com.youxilua.oschina.v.tweet.TweetView;
+import com.youxilua.utils.frgment.FragmentUtils;
+
+public class MainActivity extends FragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		FrameLayout fl = (FrameLayout) findViewById(android.R.id.custom);
+		
+		FragmentUtils.replaceDefault(android.R.id.custom, getSupportFragmentManager(), TweetApp.actionbar);
+		FragmentUtils.replaceDefault(android.R.id.empty, getSupportFragmentManager(), new TweetView());
+	
+	}
+	
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
